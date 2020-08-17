80ac419a2a67047ad06fabfb617c8358a7c82678
itsdishank <dishuhanwat007@gmail.com>
2020-08-17 12:04:43 +0530

    mod5

--- module4-solution/harder/script.js
+++ module4-solution/harder/script.js
@@ -47,7 +47,7 @@ for (var i=0;i < names.length;i++) {
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
+  // names that start with eithe23r upper case or lower case 'J'/'j', call
   // string object's 'toLowerCase' method on the result so we can compare
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
